02e7f39c602eef8287e9ebd4c2c88fe39ee62f95
Enki-lee <enki.lee@tecky.io>
2024-02-04 04:20:13 +0800

    change config

--- next.config.js
+++ next.config.js
@@ -2,15 +2,20 @@
 const nextConfig = {
     images: {
         remotePatterns: [      
-            {
-            protocol: 'https',
-            hostname: 't3.ftcdn.net',
-            port: '',
-            pathname: '/jpg/02/22/38/86/360_F_222388660_ey3if0TwU3ZyCJYUJg3AYHNrsLK4inSC.jpg',
-          },
+          //   {
+          //   protocol: 'https',
+          //   hostname: 't3.ftcdn.net',
+          //   port: '',
+          //   pathname: '/jpg/02/22/38/86/360_F_222388660_ey3if0TwU3ZyCJYUJg3AYHNrsLK4inSC.jpg',
+          // },
             {
             protocol: 'https',
             hostname: 'cdn.sanity.io',
+            port: '',
+          },
+            {
+            protocol: 'https',
+            hostname: 'www.freetobechill.com',
             port: '',
           },
         
